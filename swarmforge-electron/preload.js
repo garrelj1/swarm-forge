1b62bbdc130ebd2dc3433933c72f474f3275709a
Jeremy Garrell <garrellj101@gmail.com>
2026-05-29 17:21:52 -0400

    feat: expose onWaitingInput and onResumed on window.swarm bridge

--- swarmforge-electron/preload.js
+++ swarmforge-electron/preload.js
@@ -7,4 +7,6 @@ window.swarm = {
   writePty: (role, data) => ipcRenderer.send('pty:write', { role, data }),
   resizePty: (role, cols, rows) => ipcRenderer.send('pty:resize', { role, cols, rows }),
   cleanup: () => ipcRenderer.send('swarm:cleanup'),
+  onWaitingInput: (cb) => ipcRenderer.on('pty:waiting-input', (_, payload) => cb(payload)),
+  onResumed: (cb) => ipcRenderer.on('pty:resumed', (_, payload) => cb(payload)),
 };
